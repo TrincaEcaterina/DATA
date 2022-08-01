b101f8a29aa25728ef4445fdd5dbe50aa339463d
TrincaEcaterina <108815009+TrincaEcaterina@users.noreply.github.com>
2022-08-01 18:44:39 +0300

    Duplicates

--- clients_3.py
+++ clients_3.py
@@ -4,16 +4,15 @@
 guests_1 = [ "Bethaney Bain", "Kacey Johns", "Manpreet Saunders" ]
 guests_2 = [ "Elwood Curtis", "Diya Griffin", "Kacey Johns" ]
 guests_3 = [ "Tobey Weiss", "Kadie Barnes", "Diya Griffin" ]
-all_guests = [*guests_1, *guests_2,*guests_3]
+all_guests = guests_1 + guests_2 + guests_3
 uniq_guests= []
 
-# all_guests = guests_1 + guests_2 + guests_3
-
-# all_guests = [elem for elemnts in [guests_1,guests_2,guests_3] for elem in elemnts]
 
 for names in all_guests:
     if names not in uniq_guests:
         uniq_guests.append(names)
-print(uniq_guests)
+        uniq_guests.sort()
+        
+print('\n'.join(uniq_guests))
 
 
